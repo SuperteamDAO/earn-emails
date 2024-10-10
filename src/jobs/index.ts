ec50635253d9dfc00d5bd8d1543bd0f9f1eea6d9
abhishek <abbhishekk27@gmail.com>
2024-10-10 11:34:26 +0530

    perf: weekly listing roundup optimization

--- src/jobs/index.ts
+++ src/jobs/index.ts
@@ -30,4 +30,4 @@ if (process.env.SERVER_ENV === 'development') {
   scheduleJob('0 */6 * * *', 'createListing');
 }
 
-// scheduleJob('0 12 * * 4', 'weeklyListingRoundup');
+scheduleJob('0 12 * * 4', 'weeklyListingRoundup');
