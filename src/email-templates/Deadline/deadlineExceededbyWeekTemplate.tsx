806ee8204f51f42c1d0e02beb423e1461d581b80
Pratik Dholani <115228680+pratikdholani1@users.noreply.github.com>
2025-01-09 14:59:44 +0530

    Update deadlineExceededbyWeekTemplate.tsx

--- src/email-templates/Deadline/deadlineExceededbyWeekTemplate.tsx
+++ src/email-templates/Deadline/deadlineExceededbyWeekTemplate.tsx
@@ -20,7 +20,7 @@ export const DeadlineExceededbyWeekTemplate = ({
       <p style={styles.textWithMargin}>
         It has been a week since the <strong>{listingName}</strong> listing
         expired. The participants would be expecting the results to be out soon
-        — request you to publish the winners on Earn shortly!
+        — request you to publish the winners on Earn ASAP!
       </p>
       <p style={styles.textWithMargin}>
         <a href={link} style={styles.link}>
