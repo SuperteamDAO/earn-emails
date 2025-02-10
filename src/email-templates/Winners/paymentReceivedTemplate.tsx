ed69da823194692a8a1e82cbc355dc5145ca9da3
abhishek <abbhishekk27@gmail.com>
2025-02-10 19:43:43 +0530

    chore: changes to email copy

--- src/email-templates/Winners/paymentReceivedTemplate.tsx
+++ src/email-templates/Winners/paymentReceivedTemplate.tsx
@@ -22,13 +22,24 @@ export const PaymentReceivedTemplate = ({
       <p style={styles.greetings}>Hey {name},</p>
       <p style={styles.textWithMargin}>
         Do you know what just happened?! You just managed to turn your talent
-        into some glorious crypto.
-      </p>
-      <p style={styles.textWithMargin}>
+        into some glorious crypto.{' '}
         <a href={`${basePath}/#wallet`} style={styles.link}>
           Check your wallet on Earn
         </a>{' '}
         — you should&apos;ve received {amount} {tokenName}.
+      </p>
+      <p style={styles.textWithMargin}>
+        Wondering what you can do with your rewards? We've got you covered!{' '}
+        <a
+          href="https://superteamdao.notion.site/using-your-earn-wallet"
+          className="flex items-center text-xs font-normal text-slate-400 underline"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={styles.link}
+        >
+          Learn more here
+        </a>
+        .
       </p>
       <p style={styles.textWithMargin}>
         Also, we bet your network would love to hear about your success. Why not
