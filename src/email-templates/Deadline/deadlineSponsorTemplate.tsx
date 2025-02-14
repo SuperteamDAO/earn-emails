085959fe9f428dca3dafb3db901fb1e770e1a1bb
abhishek <abbhishekk27@gmail.com>
2025-02-14 13:25:18 +0530

    fix: formatting

--- src/email-templates/Deadline/deadlineSponsorTemplate.tsx
+++ src/email-templates/Deadline/deadlineSponsorTemplate.tsx
@@ -19,7 +19,8 @@ export const DeadlineSponsorTemplate = ({
       <p style={styles.greetings}>Hey {name},</p>
       <p style={styles.textWithMargin}>
         The deadline for your listing <strong>{listingName}</strong>
-        &nbsp; has expired. Participants would be&nbsp;expecting&nbsp;you to announce the winners on Superteam Earn within the next 5 days.
+        &nbsp; has expired. Participants would be&nbsp;expecting&nbsp;you to
+        announce the winners on Superteam Earn within the next 5 days.
       </p>
       <p style={styles.textWithMargin}>
         <a href={link} style={styles.link}>
