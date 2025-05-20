044752fc50669d7548bfcee5a3171f54e4cc191c
abhishek <abbhishekk27@gmail.com>
2025-05-20 13:45:06 +0530

    chore: change in leads email logic

--- src/handlers/Grants/leadWeeklyReminder.ts
+++ src/handlers/Grants/leadWeeklyReminder.ts
@@ -14,8 +14,6 @@ export async function processLeadWeeklyReminder() {
 
   const grantsWithFilteredApplications = await prisma.grants.findMany({
     where: {
-      airtableId: { not: null },
-      isNative: true,
       isPublished: true,
       GrantApplication: {
         some: {
