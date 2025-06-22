50742944af3a61791e02534577d51974ff9bbbf8
abhishek <abbhishekk27@gmail.com>
2025-06-22 17:12:42 +0530

    chore: update prisma deps

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,3 +1,32 @@
+import { PrismaPlanetScale } from '@prisma/adapter-planetscale';
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+const prismaClient = () => {
+  return process.env.DATABASE_URL
+    ? new PrismaClient({
+        adapter: new PrismaPlanetScale({
+          url: process.env.DATABASE_URL,
+        }),
+        omit: {
+          user: {
+            kycCountry: true,
+            kycAddress: true,
+            kycDOB: true,
+            kycIDNumber: true,
+            kycIDType: true,
+            kycName: true,
+          },
+        },
+      })
+    : new PrismaClient({
+        datasourceUrl: process.env.LOCAL_DATABASE_URL,
+      });
+};
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClient>;
+} & typeof global;
+
+export const prisma = globalThis.prismaGlobal ?? prismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
