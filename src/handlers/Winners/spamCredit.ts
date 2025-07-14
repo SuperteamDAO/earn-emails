994730201916031c14be5b1ca43014f3b43a3871
abhishek <abbhishekk27@gmail.com>
2025-07-14 12:41:39 +0530

    feat: add dispute link to spam emails

--- src/handlers/Winners/spamCredit.ts
+++ src/handlers/Winners/spamCredit.ts
@@ -1,26 +1,28 @@
 import { render } from '@react-email/render';
 
+import { basePath } from '../../constants/basePath';
 import { pratikEmail } from '../../constants/emails';
 import { SpamCreditTemplate } from '../../email-templates/Submission/SpamCreditTemplate';
 import { prisma } from '../../prisma';
 
 export async function processSpamCredit(id: string) {
-  const spammer = await prisma.submission.findUnique({
+  const spamSubmission = await prisma.submission.findUnique({
     where: { id, label: 'Spam', isActive: true, isArchived: false },
     include: { user: true, listing: true },
   });
 
-  if (spammer) {
+  if (spamSubmission) {
     const emailHtml = await render(
       SpamCreditTemplate({
-        name: spammer.user.firstName,
-        listingName: spammer.listing.title,
-        listingSlug: spammer.listing.slug,
+        name: spamSubmission.user.firstName,
+        listingName: spamSubmission.listing.title,
+        listingSlug: spamSubmission.listing.slug,
+        disputeUrl: `${basePath}/#dispute-submission-${spamSubmission.id}`,
       }),
     );
     return {
       from: pratikEmail,
-      to: spammer.user.email,
+      to: spamSubmission.user.email,
       subject: 'Your submission was flagged as spam',
       html: emailHtml,
       checkUnsubscribe: false,
